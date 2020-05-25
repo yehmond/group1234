fa49d8bc7aeb04bedcf2baf4e3bb7e379bf8410d
yehmond <yehmondray@gmail.com>
2020-05-24 23:56:56 -0700

    Setup React Router and Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App(): JSX.Element {
-    return <></>;
+    return (
+        <>
+            <Router>
+                <NavBar />
+                <Switch>
+                    <Route path="/"></Route>
+                    <Route path="/browse"></Route>
+                    <Route path="/about"></Route>
+                    <Route path="/signin"></Route>
+                    <Route path="/signup"></Route>
+                </Switch>
+            </Router>
+        </>
+    );
 }
 
 export default App;
