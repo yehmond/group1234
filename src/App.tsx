455a59a08af29121eea3c65e65cb1453f1eac023
Raymond Yeh <46778085+yehmond@users.noreply.github.com>
2020-05-27 18:10:14 -0700

    Fix compilation issues (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from "react";
-import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 
 function App(): JSX.Element {
     return (
-        <>
-            <Router>
-                <NavBar />
-                <Switch>
-                    <Route path="/"></Route>
-                    <Route path="/browse"></Route>
-                    <Route path="/about"></Route>
-                    <Route path="/signin"></Route>
-                    <Route path="/signup"></Route>
-                </Switch>
-            </Router>
-        </>
+        <Router>
+            <NavBar />
+            <Switch>
+                <Route path="/"></Route>
+                <Route path="/browse"></Route>
+                <Route path="/about"></Route>
+                <Route path="/signin"></Route>
+                <Route path="/signup"></Route>
+            </Switch>
+        </Router>
     );
 }
 
