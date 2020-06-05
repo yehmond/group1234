750313147fd2953568e5167516a54927413b4704
yongxinl <yongxinl@students.cs.ubc.ca>
2020-06-05 14:01:30 -0700

    refractored

--- src/pages/MyReservations.tsx
+++ src/pages/MyReservations.tsx
@@ -4,16 +4,12 @@ import PassReservations from "./../components/PassReservations";
 import CurrentReservation from "./../components/CurrentReservation";
 
 export default function MyReservations(): JSX.Element {
- 
-
     return (
         <div id="wrapper">
-
             <h1>My Reservations</h1>
 
-            <CurrentReservation/>
-            <PassReservations/>
-
+            <CurrentReservation />
+            <PassReservations />
         </div>
     );
 }
