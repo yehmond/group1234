780705c7660974b9a2e598627ee7862afd1ed767
jeregill <43596101+jeregill@users.noreply.github.com>
2020-06-08 17:25:24 -0700

    changed styling, adding css variables

--- src/styles/globalTheme.js
+++ src/styles/globalTheme.js
@@ -7,36 +7,74 @@ import { createMuiTheme } from "@material-ui/core";
 export const appTheme = createMuiTheme({
     palette: {
         common: {
-            black: "#000",
-            white: "#fff",
+            black: '#000',
+            white: '#fff'
         },
         background: {
-            paper: "#fff",
-            default: "#fafafa",
+            paper: '#fff',
+            'default': '#fafafa'
         },
         primary: {
-            light: "#7986cb",
-            main: "#3f51b5",
-            dark: "#303f9f",
-            contrastText: "#fff",
+            light: 'rgba(77, 184, 169, 1)',
+            main: 'rgba(0, 136, 122, 1)',
+            dark: 'rgba(0, 90, 78, 1)',
+            contrastText: '#fff'
         },
         secondary: {
-            light: "#ff4081",
-            main: "#f50057",
-            dark: "#c51162",
-            contrastText: "#fff",
+            light: 'rgba(255, 199, 102, 1)',
+            main: 'rgba(214, 150, 55, 1)',
+            dark: 'rgba(160, 104, 0, 1)',
+            contrastText: '#fff'
         },
         error: {
-            light: "#e57373",
-            main: "#f44336",
-            dark: "#d32f2f",
-            contrastText: "#fff",
+            light: 'rgba(241, 109, 109, 1)',
+            main: 'rgba(247, 28, 12, 1)',
+            dark: 'rgba(153, 22, 22, 1)',
+            contrastText: '#fff'
         },
         text: {
-            primary: "rgba(0, 0, 0, 0.87)",
-            secondary: "rgba(0, 0, 0, 0.54)",
-            disabled: "rgba(0, 0, 0, 0.38)",
-            hint: "rgba(0, 0, 0, 0.38)",
-        },
-    },
+            primary: 'rgba(0, 0, 0, 0.87)',
+            secondary: 'rgba(0, 0, 0, 0.54)',
+            disabled: 'rgba(0, 0, 0, 0.38)',
+            hint: 'rgba(0, 0, 0, 0.38)'
+        }
+    }
 });
+
+
+// export const appTheme = createMuiTheme({
+//     palette: {
+//         common: {
+//             black: "#000",
+//             white: "#fff",
+//         },
+//         background: {
+//             paper: "#fff",
+//             default: "#fafafa",
+//         },
+//         primary: {
+//             light: "#7986cb",
+//             main: "#3f51b5",
+//             dark: "#303f9f",
+//             contrastText: "#fff",
+//         },
+//         secondary: {
+//             light: "#ff4081",
+//             main: "#f50057",
+//             dark: "#c51162",
+//             contrastText: "#fff",
+//         },
+//         error: {
+//             light: "#e57373",
+//             main: "#f44336",
+//             dark: "#d32f2f",
+//             contrastText: "#fff",
+//         },
+//         text: {
+//             primary: "rgba(0, 0, 0, 0.87)",
+//             secondary: "rgba(0, 0, 0, 0.54)",
+//             disabled: "rgba(0, 0, 0, 0.38)",
+//             hint: "rgba(0, 0, 0, 0.38)",
+//         },
+//     },
+// });
