73a5aadd4b4f1ec371990ca55c1e494df725adbf
yongxinl <yongxinl@students.cs.ubc.ca>
2020-07-24 15:36:25 -0700

    still need to fix calls to api

--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,  } from "react";
+import React, { useEffect } from "react";
 // TODO: import useState
 import { useParams } from "react-router-dom";
 import Box from "@material-ui/core/Box";
